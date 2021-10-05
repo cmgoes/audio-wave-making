09a0489fb59ec842b9d116833b7af4a0958403ba
HowlingNitin <maletiger000@gmail.com>
2021-10-05 13:56:47 +0200

    update audio recording

--- front-end/src/views/ControlPanel/Audios/AudioRecording.js
+++ front-end/src/views/ControlPanel/Audios/AudioRecording.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import classNames from "classnames";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/control-panel.js";
-import { Backdrop, Fade, Grid, IconButton, Link, Modal, Paper, Snackbar, TextField, Typography } from "@material-ui/core";
+import { Backdrop, Fade, Grid, Link, Modal, Paper, Snackbar, Typography } from "@material-ui/core";
 import { Lock, Stop } from "@material-ui/icons";
 import MuiAlert from '@material-ui/lab/Alert';
 import { Axios } from "redux/services";
-// import { ReactMic } from 'react-mic';
 import { audioList } from "redux/actions/audio";
-const MicRecorder = require('mic-recorder-to-mp3');
+import { ReactMediaRecorder, useReactMediaRecorder } from "react-media-recorder";
 
 const useStyles = makeStyles(styles);
 
@@ -21,9 +20,6 @@ function Alert(props) {
 export default function AudioRecording({ open, handleOpenModal }) {
     const classes = useStyles();
     const dispatch = useDispatch();
-    const recorder = new MicRecorder({
-        bitRate: 128
-    });
 
     const [openAlert, setOpenAlert] = useState({
         open: false,
@@ -37,14 +33,12 @@ export default function AudioRecording({ open, handleOpenModal }) {
     useEffect(() => {
     }, [couter])
 
-    const startRecording = () => {
+    const startAudioRecording = () => {
         setBeforeRecord(true);
         var couterInterval = setInterval(() => {
             setCouter(e => {
                 if (e === 1) {
-                    recorder.start().then(() => {
-                        setRecord(true);
-                    });
+                    setRecord(true);
                     return clearInterval(couterInterval);
                 }
                 return e - 1
@@ -53,25 +47,8 @@ export default function AudioRecording({ open, handleOpenModal }) {
         // setRecord(true)
     }
 
-    const stopRecording = () => {
+    const stopAudioRecording = () => {
         setRecord(false);
-        recorder.stop().getMp3().then(([buffer, blob]) => {
-                // do what ever you want with buffer and blob
-                // Example: Create a mp3 file and play
-                // const file = new File(buffer, 'me-at-thevoice.mp3', {
-                //     type: blob.type,
-                //     lastModified: Date.now()
-                // });
-
-                console.log(`buffer`, buffer, blob)
-
-                // const player = new Audio(URL.createObjectURL(file));
-                // player.play();
-
-            }).catch((e) => {
-                alert('We could not retrieve your message');
-                console.log(e);
-            });
     }
 
     const onStop = async (recordedBlob) => {
@@ -88,13 +65,13 @@ export default function AudioRecording({ open, handleOpenModal }) {
 
         const formData = new FormData();
         formData.append("audio", recordedBlob.blob, 'Recording.mp3')
-
-        const response = await Axios({ url: 'api/audio/uploadRecording', data: formData })
-        if (response.status) {
-            setOpenAlert({ ...openAlert, open: true, status: "success", text: "An audio file has been uploaded successfully!" })
-            dispatch(audioList(response.data))
-            handleOpenModal()
-        }
+        console.log(`formData`, formData)
+        // const response = await Axios({ url: 'api/audio/uploadRecording', data: formData })
+        // if (response.status) {
+        //     setOpenAlert({ ...openAlert, open: true, status: "success", text: "An audio file has been uploaded successfully!" })
+        //     dispatch(audioList(response.data))
+        //     handleOpenModal()
+        // }
     }
 
     return (
@@ -114,27 +91,36 @@ export default function AudioRecording({ open, handleOpenModal }) {
                 <Fade in={open} >
                     <Paper className={classes.modalBody}>
                         <Grid container alignItems="center" direction="column">
-                            {/* <ReactMic
-                                record={record}
-                                onStop={onStop}
-                                className={classes.dHidden}
-                                mimeType="audio/mp3"
-                            /> */}
-                            <div className={classes.micAllow}>
+                        {
+                            navigator.mediaDevices.getUserMedia({video: false, audio: true}).then(() => (
+                                <ReactMediaRecorder
+                                    audio
+                                    render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                                        <div>
+                                            <p>{status}</p>
+                                            <button onClick={startRecording}>Start Recording</button>
+                                            <button onClick={stopRecording}>Stop Recording</button>
+                                            <audio src={mediaBlobUrl} controls autoPlay loop />
+                                        </div>
+                                    )}
+                                />
+                            ))
+                        }
+                            {/* <div className={classes.micAllow}>
                                 {
                                     beforeRecord ?
                                         <Typography variant="h1" style={{ fontSize: "100px", fontFamily: "serif", fontStyle: "italic" }}>{couter}</Typography>
                                         :
-                                        <Lock style={{ fontSize: "100px" }} onClick={startRecording} />
+                                        <Lock style={{ fontSize: "100px" }} onClick={startAudioRecording} />
                                 }
                                 {
-                                    record ? <Stop style={{ fontSize: "100px" }} onClick={stopRecording} /> : null
+                                    record ? <Stop style={{ fontSize: "100px" }} onClick={stopAudioRecording} /> : null
                                 }
-                            </div>
+                            </div> */}
                             <Typography variant="h2" style={{ fontWeight: "bold", fontSize: '27px' }} className={classNames(classes.mv10, classes.ph30)}>Click the lock to allow</Typography>
                             <Typography variant="caption" align="center">We need permission to use your microphone.</Typography>
                             <Typography variant="caption" align="center">Click on the lock to give us permission.</Typography>
-                            <Link align="center" className={classes.mt10} href="#" onClick={stopRecording}>
+                            <Link align="center" className={classes.mt10} href="#" onClick={stopAudioRecording}>
                                 Cancel
                             </Link>
                         </Grid>
